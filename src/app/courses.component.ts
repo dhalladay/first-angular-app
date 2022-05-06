8ede9d5e3a94adfabdf3a5623565abf16a646eee
daveh <dave.halladay@outlook.com>
2022-05-05 23:21:10 -0600

    create angular app and create first comp, courese

---
+++ src/app/courses.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'; 
+
+@Component({
+  selector: 'courses', // <div id="courses"> your selector would be "#courses" use '.' for class. Basic CSS selectors
+  template: '<h2>Courses<h2>'
+})
+export class CoursesComponent {
+
+}
